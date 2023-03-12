623ae057e4f96ca32b11e15d2a5bebcf972bbbec
Mykola Matvyeyev <mnasoft@gmail.com>
2023-03-12 20:05:45 +0200

    Sun 12 Mar 2023 06:05:45 PM UTC

--- package.lisp
+++ package.lisp
@@ -1,9 +1,7 @@
 ;;;; package.lisp
 
-(defpackage :acad-formats)
-
 (defpackage :acad-formats
   (:use #:cl)
   (:export acad-formats::a-formats))
 
-;;;; (declaim (optimize (compilation-speed 0) (debug 3) (safety 0) (space 0) (speed 0)))
+(in-package :acad-formats)
